0f70a2bd4497b25aaf84e38aab907f41db3f68e1
mansijmaharzn <mansijmaharzn@gmail.com>
2023-07-17 16:11:05 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import telebot
+from scraper import CompanyDetailsScraper
+
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+scraper = CompanyDetailsScraper()
+
+
+# replies for /start and /hello
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    text = "Howdy, how are you doing?\nI'm *NEPSE-ALERTER*!\nMy god is *Sir Mansij Maharjan*"
+    bot.reply_to(message, text, parse_mode="Markdown")
+
+
+# main functionalities
+def scraper_handler(message):
+    query_symbol = message.text.upper()
+    symbol_details = scraper.scrape_company_details(query_symbol)
+
+    symbol_message = f"Results for: " + "\n".join(symbol_details)
+    bot.send_message(message.chat.id, symbol_message, parse_mode="Markdown")
+
+
+@bot.message_handler(commands=['search', 'get', 'show', 'display'])
+def get_company_details(message):
+    command_params = message.text.split()[1:]
+
+    text = "Send Company Symbol to get details of:\nExample: *GVL*, *NABIL*, *NIFRA*, *CITY*"
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, scraper_handler)
+
+
+bot.infinity_polling()
---
+++ scraper.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+
+class CompanyDetailsScraper:
+    def __init__(self):
+        self.base_url = "https://merolagani.com/CompanyDetail.aspx?symbol="
+        
+
+    def get_company_details(self, symbol):
+        url = self.base_url + symbol
+    
+        try:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            return soup
+        except requests.exceptions.RequestException as e:
+            print("Error occurred during the request:", e)
+            return None
+    
+    def parse_company_general_details(self, soup):
+        company_full_name = soup.find('h4', class_="company-inner-title")
+        company_full_name = f"*{company_full_name.get_text(strip=True)}*\n"
+        tbodies = soup.find_all('tbody')
+        
+        symbol_details = [company_full_name]
+        for tbody in tbodies:
+            tr = tbody.find('tr')
+            th = tr.find('th') # heading
+            td = tr.find('td') # value
+    
+            if th.text.strip() == "% Dividend":
+                break
+    
+            td_text = td.get_text(strip=True)
+            symbol_details.append(f"{th.text.strip()}: `{td_text}`")
+
+            # invalid symbl error to be handled
+            # if not td_text:
+            #     symbol_details.append("Invalid Symbol")
+            #     break
+            # else:
+            #     symbol_details.append(f"{th.text.strip()}: `{td_text}`")
+        
+        return symbol_details
+    
+    def scrape_company_details(self, query_symbol):
+        soup = self.get_company_details(query_symbol)
+        
+        if soup:
+            return self.parse_company_general_details(soup)
